1923747e2ef96cb6a2b0f390d0bf36b74d97f1dc
Jeep0926 <tatsuro.0926@icloud.com>
2025-05-20 17:56:03 +0900

    add:FastAPI（Python）のデプロイ完了

--- Gemfile
+++ Gemfile
@@ -15,14 +15,14 @@ gem 'pg', '~> 1.1'
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '>= 5.0'
 
-gem 'jsbundling-rails'
+# gem 'jsbundling-rails'
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem 'turbo-rails'
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem 'stimulus-rails'
 # Bundle and process CSS [https://github.com/rails/cssbundling-rails]
-gem 'cssbundling-rails'
+# gem 'cssbundling-rails'
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem 'jbuilder'
 
@@ -67,3 +67,7 @@ group :development do
 end
 
 gem 'tailwindcss-rails'
+
+gem "dockerfile-rails", ">= 1.7", :group => :development
+
+gem "aws-sdk-s3", require: false
--- config/puma.rb
+++ config/puma.rb
@@ -9,38 +9,33 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
-min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
-threads min_threads_count, max_threads_count
+# Puma のスレッド／ワーカー数を環境変数で制御
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 1))
+threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
+threads threads_count, threads_count
 
-rails_env = ENV.fetch('RAILS_ENV', 'development')
+rails_env = ENV.fetch("RAILS_ENV", "development")
 
-if rails_env == 'production'
-  # If you are running more than 1 thread per process, the workers count
-  # should be equal to the number of processors (CPU cores) in production.
-  #
-  # It defaults to 1 because it's impossible to reliably detect how many
-  # CPU cores are available. Make sure to set the `WEB_CONCURRENCY` environment
-  # variable to match the number of processors.
-  worker_count = Integer(ENV.fetch('WEB_CONCURRENCY', 1))
-  if worker_count > 1
-    workers worker_count
-  else
-    preload_app!
-  end
+# 本番環境ではプリロードし、ワーカー数に応じてフォーク
+preload_app! if rails_env == "production"
+
+# フォーク後の DB 接続再確立
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
 end
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch('PORT', 3000)
+# ポート設定とバインド（0.0.0.0 でリッスン）
+# IPv4 + IPv6 両対応にする場合は第2引数に "::" を渡す
+port ENV.fetch("PORT", 3000)
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}" 
 
-# Specifies the `environment` that Puma will run in.
 environment rails_env
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+# タイムアウト設定（開発のみ）
+worker_timeout 3600 if rails_env == "development"
 
-# Allow puma to be restarted by `bin/rails restart` command.
+# PID ファイル
+pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
+
+# bin/rails restart を許可
 plugin :tmp_restart
