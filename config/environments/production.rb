a473b9b0692c81640df45fb206265664dea5399b
Jeep0926 <tatsuro.0926@icloud.com>
2025-05-21 11:02:19 +0900

    add:FastAPIのデプロイ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << /\A.*\.fly\.io\z/   # fly.io のサブドメインを許可
+  config.hosts << /\Afdaa:.*\z/  # cSpell:ignore fdaa
 end
