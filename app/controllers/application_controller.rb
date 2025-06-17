c954b32af638bc5b2601d40ed9fc4453d44b972d
Jeep0926 <tatsuro.0926@icloud.com>
2025-06-18 08:58:52 +0900

    add:共通下部ナビゲーションを実装し、表示制御ロジックを導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def hello
-    render html: 'Hello from VoiceBloom on Docker (Rails server is running)!'
+  before_action :set_show_bottom_nav
+
+  private
+
+  # デフォルトで下部ナビゲーションを表示する設定
+  def set_show_bottom_nav
+    @show_bottom_nav = true
   end
 end
