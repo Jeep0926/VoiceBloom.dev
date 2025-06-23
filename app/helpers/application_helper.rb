87821f9b027364f53dffd807148023865f06187d
Jeep0926 <tatsuro.0926@icloud.com>
2025-06-23 13:44:33 +0900

    update:共通ヘッダーの「戻る」ボタンの挙動と表示を統一するよう更新

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,19 @@ module ApplicationHelper
       'text-gray-500 hover:text-purple-600' # 非アクティブ時のCSSクラス
     end
   end
+
+  # ヘッダー用の「ホームへ戻る」リンクを生成するヘルパー
+  def home_back_link
+    # link_to ヘルパーを使って、ログイン後のルートパスへのリンクを作成
+    link_to authenticated_root_path, class: 'text-gray-600 p-2 flex items-center' do
+      # 戻るアイコンのSVG
+      svg_icon = content_tag(:svg, class: 'h-6 w-6', xmlns: 'http://www.w3.org/2000/svg', fill: 'none',
+                                   viewBox: '0 0 24 24', 'stroke-width': '2.5', stroke: 'currentColor') do
+        content_tag(:path, nil, 'stroke-linecap': 'round', 'stroke-linejoin': 'round', d: 'M15.75 19.5L8.25 12l7.5-7.5')
+      end
+
+      # アイコンとテキストを結合して返す
+      svg_icon + content_tag(:span, 'ホームへ', class: 'text-sm font-medium ml-1')
+    end
+  end
 end
