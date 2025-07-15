07d668eae2fc6c2d316788a2bd89e813e6a7ad7c
Jeep0926 <tatsuro.0926@icloud.com>
2025-07-16 07:11:30 +0900

    fixed:Renderの環境変数を使ってDBリセットの操作をON/OFFできるように修正

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,13 +4,21 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-# production環境の場合のみ、データベースのマイグレーションとseedを実行
+# production環境の場合のみDB操作を実行
 if [ "$RAILS_ENV" = "production" ]; then
-  echo "Running database migrations..."
-  bundle exec rails db:migrate
+  # DB_RESETフラグが"true"の場合、DBをリセット
+  if [ "$DB_RESET" = "true" ]; then
+    echo ">>> Resetting database based on DB_RESET flag..."
+    bundle exec rails db:reset # このコマンドがDBのdrop, create, schema:load, seedを実行します
+    echo ">>> Database reset complete."
+  # それ以外の場合は、通常のマイグレーションとseedを実行
+  else
+    echo ">>> Running database migrations..."
+    bundle exec rails db:migrate
 
-  echo "Seeding database if necessary..."
-  bundle exec rails db:seed
+    echo ">>> Seeding database if necessary..."
+    bundle exec rails db:seed
+  fi
 fi
 
 # DockerfileのCMDで渡されたコマンドを実行
